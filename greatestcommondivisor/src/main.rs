1bf4ddc12201a8c5fddb3f1329879ce16a5c02b7
michi-jeremias <michael.jeremias.home@gmail.com>
2022-11-16 22:37:36 +0100

    init

---
+++ greatestcommondivisor/src/main.rs
@@ -0,0 +1,18 @@
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
+
+fn main() {
+    println!("Hello, world!");
+    let result = gcd(7, 35);
+    println!("result is {}", result);
+}
---
+++ greatestcommondivisor/target/debug/greatestcommondivisor.d
@@ -0,0 +1,1 @@
+/home/michi/dev/rust/learningrust/greatestcommondivisor/target/debug/greatestcommondivisor: /home/michi/dev/rust/learningrust/greatestcommondivisor/src/main.rs
