4338fff606031c92074b73dfce980e8b62a7b0d4
michi-jeremias <michael.jeremias.home@gmail.com>
2022-11-17 08:06:09 +0100

    .gitignore

--- greatestcommondivisor/target/debug/deps/greatestcommondivisor-1727e7d31d19acf6.d
+++
@@ -1,5 +0,0 @@
-/home/michi/dev/rust/learningrust/greatestcommondivisor/target/debug/deps/greatestcommondivisor-1727e7d31d19acf6: src/main.rs
-
-/home/michi/dev/rust/learningrust/greatestcommondivisor/target/debug/deps/greatestcommondivisor-1727e7d31d19acf6.d: src/main.rs
-
-src/main.rs:
--- greatestcommondivisor/target/debug/greatestcommondivisor.d
+++
@@ -1,1 +0,0 @@
-/home/michi/dev/rust/learningrust/greatestcommondivisor/target/debug/greatestcommondivisor: /home/michi/dev/rust/learningrust/greatestcommondivisor/src/main.rs
