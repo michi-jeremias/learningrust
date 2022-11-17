2ee0874a77ea7863fe1a6d9abc0694a90c134dd1
michi-jeremias <michael.jeremias.home@gmail.com>
2022-11-17 22:22:13 +0100

    mandelbrot 1

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -1,3 +1,26 @@
+extern crate num;
+use num::Complex;
+
+/// Try to determine if `c` is in the Mandelbrot set, using at most `limit`
+/// iterations to decide.
+///
+/// If `c` is not a member, return `Some(i)`, where `i` is the number of
+/// iterations it took for `c` to leave the circle of radius two centered on the
+/// origin. If `c` seems to be a member (more precisely, if we reached the
+/// iteration limit without being able to prove that `c` is not a member),
+/// return `None`.
+#[allow(dead_code)]
+fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
+    let mut z = Complex {re: 0.0, im: 0.0};
+   for i in 0..limit {
+        z = z * z + c;
+        if z.norm_sqr() > 4.0 {
+            return Some(i)
+        }
+    }
+    None
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
