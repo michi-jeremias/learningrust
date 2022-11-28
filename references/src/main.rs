0d21703b52cd8478d35c2805b75e09e8d71916c0
michi-jeremias <michael.jeremias.home@gmail.com>
2022-11-28 22:48:58 +0100

    references

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,3 +1,20 @@
+use std::collections::HashMap;
+type Table = HashMap<String, Vec<String>>;
+
+fn show(table: &Table) {
+    for (artist, works) in table {
+        println!("works by: {}:", artist);
+        for work in works {
+            println!(" {}", work);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut table = Table::new();
+    table.insert("Gesualdo".to_string(), vec!["many madrigals".to_string(), "Tenebrae responsoria".to_string()]);
+    table.insert("Caravaggio".to_string(), vec!["The Musicians".to_string(), "The calling of St. Mathew".to_string()]);
+    table.insert("Cellini".to_string(), vec!["Persues with the head of medusa".to_string(), "a salt cellar".to_string()]);
+
+    show(&table);
 }
